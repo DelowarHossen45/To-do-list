cc2f3c72f130300cc2ceae75a9bdeeefcf51b357
Apple Mahmud <applecse25@gmail.com>
2018-04-18 23:24:07 +0600

    header file initialization

---
+++ labwork.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include<fstream>
+using namespace std;
+struct list
+{
+	string name;
+   string description;
+   string Date;
+};
+struct list data[100];
+bool Add_in_file(string name,string description, string Date)
+{
+	
+   if (listSize < 101)
+   {
+      list y = { name,description, Date };
+      data[listSize] = y;
+      listSize++;
+      return true;
+   }
+   else
+   {
+      return false;
+ 
+ 
+   }
+}
